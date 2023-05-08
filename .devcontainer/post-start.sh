c3b8e3f0c2d42dcdcc43e4d1d81436f9d6f38212
bartr <bartr@microsoft.com>
2023-05-08 02:17:51 +0000

    use :beta tag

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -8,8 +8,8 @@ echo "$(date +'%Y-%m-%d %H:%M:%S')    post-start start" >> "$HOME/status"
 echo "Pulling docker images"
 docker pull mcr.microsoft.com/dotnet/sdk:6.0
 docker pull mcr.microsoft.com/dotnet/aspnet:6.0-alpine
-docker pull ghcr.io/cse-labs/res-edge-webv:0.8
-docker pull ghcr.io/cse-labs/res-edge-automation:0.8
+docker pull ghcr.io/cse-labs/res-edge-webv:beta
+docker pull ghcr.io/cse-labs/res-edge-automation:beta
 
 echo "post-start complete"
 echo "$(date +'%Y-%m-%d %H:%M:%S')    post-start complete" >> "$HOME/status"
