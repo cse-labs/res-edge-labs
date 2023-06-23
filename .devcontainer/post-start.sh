6c87237666cb782c4f0cf57f89b4ae1ea18324ad
bartr <bartr@microsoft.com>
2023-06-23 23:05:25 +0000

    updated deploy

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -8,8 +8,8 @@ echo "$(date +'%Y-%m-%d %H:%M:%S')    post-start start" >> "$HOME/status"
 echo "Pulling docker images"
 docker pull mcr.microsoft.com/dotnet/sdk:7.0
 docker pull mcr.microsoft.com/dotnet/aspnet:7.0-alpine
-docker pull ghcr.io/cse-labs/res-edge-webv:0.9
-docker pull ghcr.io/cse-labs/res-edge-automation:0.9
+docker pull ghcr.io/cse-labs/res-edge-webv0.12
+docker pull ghcr.io/cse-labs/res-edge-automation0.12
 
 echo "post-start complete"
 echo "$(date +'%Y-%m-%d %H:%M:%S')    post-start complete" >> "$HOME/status"
