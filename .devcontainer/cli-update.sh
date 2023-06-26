d78c51a62dac35bb91780d1ccfa447adcc4e9966
bartr <bartr@microsoft.com>
2023-06-26 05:39:59 +0000

    updated cli

--- .devcontainer/cli-update.sh
+++ .devcontainer/cli-update.sh
@@ -14,27 +14,27 @@ rm -rf kic .kic flt .flt ds .ds
 
 # use latest release
 if [ "$tag" = "" ]; then
-    tag=$(curl -s https://api.github.com/repos/kubernetes101/pib-dev/releases/latest | grep tag_name | cut -d '"' -f4)
+    tag=$(curl -s https://api.github.com/repos/cse-labs/res-edge-labs/releases/latest | grep tag_name | cut -d '"' -f4)
 fi
 
 # install kic
-wget -O kic.tar.gz "https://github.com/kubernetes101/pib-dev/releases/download/$tag/kic-$tag-linux-amd64.tar.gz"
+wget -O kic.tar.gz "https://github.com/cse-labs/res-edge-labs/releases/download/$tag/kic-$tag-linux-amd64.tar.gz"
 tar -xvzf kic.tar.gz
 rm kic.tar.gz
 
 # install flt
-wget -O flt.tar.gz "https://github.com/kubernetes101/pib-dev/releases/download/$tag/flt-$tag-linux-amd64.tar.gz"
-tar -xvzf flt.tar.gz
-rm flt.tar.gz
+# wget -O flt.tar.gz "https://github.com/cse-labs/res-edge-labs/releases/download/$tag/flt-$tag-linux-amd64.tar.gz"
+# tar -xvzf flt.tar.gz
+# rm flt.tar.gz
 
 # install ds
-wget -O ds.tar.gz "https://github.com/kubernetes101/pib-dev/releases/download/$tag/ds-$tag-linux-amd64.tar.gz"
+wget -O ds.tar.gz "https://github.com/cse-labs/res-edge-labs/releases/download/$tag/ds-$tag-linux-amd64.tar.gz"
 tar -xvzf ds.tar.gz
 rm ds.tar.gz
 
 # update completions
 kic completion zsh > "$HOME/.oh-my-zsh/completions/_kic"
-flt completion zsh > "$HOME/.oh-my-zsh/completions/_flt"
+# flt completion zsh > "$HOME/.oh-my-zsh/completions/_flt"
 ds completion zsh > "$HOME/.oh-my-zsh/completions/_ds"
 
 cd "$OLDPWD" || exit
