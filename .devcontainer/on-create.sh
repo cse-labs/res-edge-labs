a3cb630e6ee417a55cbcb57167a836ec09d1e159
bartr <bartr@microsoft.com>
2023-11-17 06:11:10 +0000

    merged 0.15

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -107,8 +107,8 @@ kic cluster create
 echo "Pulling docker images"
 docker pull mcr.microsoft.com/dotnet/sdk:7.0
 docker pull mcr.microsoft.com/dotnet/aspnet:7.0-alpine
-docker pull ghcr.io/cse-labs/res-edge-webv:0.14
-docker pull ghcr.io/cse-labs/res-edge-automation:0.14
+docker pull ghcr.io/cse-labs/res-edge-webv:0.15
+docker pull ghcr.io/cse-labs/res-edge-automation:0.15
 
 sudo apt-get update
 
